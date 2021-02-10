39446d19f555f32ccbb5eb4b635e722e74cf86c9
Raymon de Looff <raydelooff@gmail.com>
2021-02-10 10:39:54 +0100

    Bump version

--- recranet.php
+++ recranet.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Recranet
  * Plugin URI:        https://docs.recranet.com/
  * Description:       Recranet Booking Elements
- * Version:           1.2.0
+ * Version:           1.2.1
  * Author:            Recranet
  * Author URI:        https://recranet.com/
  * Text Domain:       recranet
